0fe30c5e1bc13ce99b2280c727c244d65d401003
nhimdodev <91662205+hoaidev1704@users.noreply.github.com>
2022-10-12 15:25:00 +0700

    Create WebService Drawler

---
+++ Drawler/src/main/java/nlu/dw/drawler/DrawlerApplication.java
@@ -0,0 +1,13 @@
+package nlu.dw.drawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawlerApplication.class, args);
+	}
+
+}
