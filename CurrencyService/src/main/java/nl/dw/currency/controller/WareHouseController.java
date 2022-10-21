16f3063dd8ba03f141bfea047befb22790eb40b1
nhimdodev <91662205+hoaidev1704@users.noreply.github.com>
2022-10-22 00:43:54 +0700

    commit

--- CurrencyService/src/main/java/nl/dw/currency/controller/WareHouseController.java
+++ CurrencyService/src/main/java/nl/dw/currency/controller/WareHouseController.java
@@ -6,7 +6,6 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class WareHouseController {
