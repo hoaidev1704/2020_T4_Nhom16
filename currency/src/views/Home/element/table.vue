eb51290aa1c14a1b4d0d62996cfa0019ca175c43
T.Nhi <t.nhi@tego.global>
2022-11-20 15:59:16 +0700

    dowload file csv

--- currency/src/views/Home/element/table.vue
+++ currency/src/views/Home/element/table.vue
@@ -5,16 +5,22 @@
     :columns="columns"
     :data-source="dataTable"
   >
-  <template #id="{ text }">
+    <template #id="{ text }">
       <a>{{ text }}</a>
-  </template>
-  <template #bank="{ text }">
-      <a :id=text.id>{{ text.name }}</a>
-  </template>
-  <template #currency="{ text }">
-      <a :id=text.id>{{text.symbol}} - {{ text.name }}</a>
-  </template>
+    </template>
+    <template #bank="{ text }">
+      <a :id="text.id">{{ text.name }}</a>
+    </template>
+    <template #currency="{ text }">
+      <a :id="text.id">{{ text.symbol }} - {{ text.name }}</a>
+    </template>
   </a-table>
+  <a-button
+    type="primary"
+    class="text-left mt-5"
+    @click="handleDowload(dataTable)"
+    >Dowload</a-button
+  >
 </template>
 <script lang="ts">
 import { NumberDecimal } from "ant-design-vue/lib/input-number/src/utils/MiniDecimal";
@@ -22,38 +28,41 @@ import { defineComponent, onMounted, ref } from "vue";
 import { getDataTable } from "../../../api/TableAPI/table";
 
 const columns = [
-  { title: "Id", dataIndex: "id" , slots: { customRender: 'id' }},
+  { title: "Id", dataIndex: "id", slots: { customRender: "id" } },
+  { title: "Bank Name", dataIndex: "bank", slots: { customRender: "bank" } },
+  {
+    title: "Name Currency",
+    dataIndex: "currency",
+    slots: { customRender: "currency" },
+  },
+  { title: "Price", dataIndex: "price" },
+  { title: "Buy Cash", dataIndex: "buyCash" },
+  { title: "Buy Transfer", dataIndex: "buyTransfer" },
   { title: "Get Date", dataIndex: "getDate" },
   { title: "Update Date", dataIndex: "updateDate" },
   { title: "Url Source", dataIndex: "urlSource" },
-  { title: "Buy Cash", dataIndex: "buyCash" },
-  { title: "Buy Transfer", dataIndex: "buyTransfer" },
-  { title: "Price", dataIndex: "price" },
-  { title: "Bank Name", dataIndex: "bank", slots: { customRender: 'bank' } },
-  { title: "Name Currency", dataIndex: "currency", slots: { customRender: 'currency' } },
 ];
 interface Bank {
-  id:string,
-  name:string
+  id: string;
+  name: string;
 }
 
 interface Currency {
-  id:string,
-  symbol:string,
-  name:string
+  id: string;
+  symbol: string;
+  name: string;
 }
 
 interface Exchange {
-  id:string,
-  getDate:string,
-  updateDate:string,
-  url:string,
-  buyCash:string,
-  buyTransfer:string,
-  price:NumberDecimal,
-  bank:Bank
-  currency:Currency
-
+  id: string;
+  getDate: string;
+  updateDate: string;
+  url: string;
+  buyCash: string;
+  buyTransfer: string;
+  price: NumberDecimal;
+  bank: Bank;
+  currency: Currency;
 }
 
 export default defineComponent({
@@ -68,9 +77,22 @@ export default defineComponent({
     onMounted(() => {
       getData();
     });
+    const handleDowload = (data: any) => {
+      let csv = "";
+      data.forEach(function (row: any) {
+        csv += row.join(",");
+        csv += "\n";
+      });    
+      const hiddenElement = document.createElement("a");
+      hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+      hiddenElement.target = "_blank";
+      hiddenElement.download = `dowload.csv`;
+      hiddenElement.click();
+    };
     return {
       columns,
       dataTable,
+      handleDowload,
     };
   },
 });
